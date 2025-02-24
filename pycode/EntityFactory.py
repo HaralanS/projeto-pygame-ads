2b29f895abc0adc2bd5dcb56888352cbc66acf47
HaralanS <haralansantana@gmail.com>
2025-02-24 14:03:43 -0300

    create enemies and players

--- pycode/EntityFactory.py
+++ pycode/EntityFactory.py
@@ -1,5 +1,8 @@
+import random
 from pycode.Background import Background
-from pycode.Const import WIN_WIDTH
+from pycode.Const import WIN_HEIGHT, WIN_WIDTH
+from pycode.Enemy import Enemy
+from pycode.Player import Player
 
 
 class EntityFactory:
@@ -14,3 +17,15 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+            
+            case 'Player1':
+                return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
+            
+            case 'Player2':
+                return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
+            
+            case 'Enemy1':
+                return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(20, WIN_HEIGHT - 20)))
+            
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(20, WIN_HEIGHT - 20)))
